bf60d8b8d39fab6f38307dee2c835abadefea3b4
NLluuk <luuk.wagenaar01@hotmail.com>
2020-08-17 17:02:08 +0200

    Added documentation

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-# driver = webdriver.Chrome(
-#     r'C:\Users\Luuk\Desktop\whatsapp bot\chromedriver.exe')
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
@@ -8,11 +5,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
-contact = "Valerie"
-text = "Hi Val hahaha"
+# Insert name of your target between the "" (Must be in your contacts list!)
+contact = ""
+# Insert the text you want to send them between the ""
+text = ""
+# Change to driver = webdriver.Firefox() if you dont want to use Chrome
 driver = webdriver.Chrome()
 driver.get("https://web.whatsapp.com")
-print("Scan QR Code, And then Enter")
+print("Scan QR Code, And then hit Enter in the console")
 input()
 print("Logged In")
 input_box_search = WebDriverWait(driver, 10).until(
@@ -26,8 +26,7 @@ selected_contact.click()
 inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
 input_box = driver.find_element_by_xpath(inp_xpath)
 time.sleep(2)
-for i in range(25):
-    #input_box.send_keys(text + str(i) + Keys.ENTER)
+for i in range(25):  # specify number of messages here
     input_box.send_keys(text + Keys.ENTER)
-time.sleep(2)
+time.sleep(10)
 driver.quit()
